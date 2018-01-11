7f2abbb2c415d0a058387606ef92c35984071fcb
yazeedb <ybzadough@gmail.com>
2018-01-10 20:49:55 -0500

    move babel config to .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -28,11 +28,7 @@ module.exports = {
 				test: /\.js$/,
 				exclude: /node_modules/,
 				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env', '@babel/preset-react'],
-						plugins: ['@babel/plugin-proposal-object-rest-spread']
-					}
+					loader: 'babel-loader'
 				}
 			},
 			{
